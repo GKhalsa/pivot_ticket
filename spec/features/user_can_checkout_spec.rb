5a636088d1825f7a1cc4e117a56a50a89dd3231d
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-05-11 12:25:53 -0600

    Restore user can checkout spec

--- spec/features/user_can_checkout_spec.rb
+++ spec/features/user_can_checkout_spec.rb
@@ -1,29 +1,29 @@
-# require "rails_helper"
-#
-# RSpec.feature "User can checkout" do
-#   scenario "they see a flash message" do
-#     user = create(:user)
-#
-#     ApplicationController.any_instance.stubs(:current_user).returns(user)
-#
-#     create_list(:item, 2)
-#
-#     item_1 = Item.first
-#     item_2 = Item.last
-#
-#     visit items_path
-#     within(".card-#{item_1.id}") do
-#       click_button("Add to Cart")
-#     end
-#
-#     within(".card-#{item_2.id}") do
-#       click_button("Add to Cart")
-#     end
-#
-#     visit cart_path
-#
-#     click_on "Checkout"
-#
-#     expect(page).to have_content("Order was successfully placed")
-#   end
-# end
+require "rails_helper"
+
+RSpec.feature "User can checkout" do
+  scenario "they see a flash message" do
+    user = create(:user)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    create_list(:ticket, 2)
+
+    ticket_1 = Ticket.first
+    ticket_2 = Ticket.last
+
+    visit tickets_path
+    within(".card-#{ticket_1.id}") do
+      click_button("Add to Cart")
+    end
+
+    within(".card-#{ticket_2.id}") do
+      click_button("Add to Cart")
+    end
+
+    visit cart_path
+
+    click_on "Checkout"
+
+    expect(page).to have_content("Order was successfully placed")
+  end
+end
