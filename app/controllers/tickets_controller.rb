62f620fb493352148e2de512cdd7b67a59adedd9
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-05-11 16:11:24 -0600

    Add partial for venue links

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,5 +2,6 @@ class TicketsController < ApplicationController
   def index
     @categories = Category.all
     @tickets = Ticket.all_by_state
+    @venues = Venue.all
   end
 end
--- spec/features/user_sees_events_by_venue_spec.rb
+++ spec/features/user_sees_events_by_venue_spec.rb
@@ -4,14 +4,19 @@ RSpec.feature "User sees events by venue" do
   context "They visit the venue's event page" do
     scenario "They only see the events of that venue" do
       venue_1, venue_2 = create_list(:venue, 2)
+      event_1, event_2 = create_list(:event, 2, venue_id: venue_1.id)
+      event_3 = create(:event, venue_id: venue_2.id)
 
       visit root_path
 
+      click_on venue_1.name
 
-      # I should see links to tickets sorted by venue.
-      # when I click on a venue, I should be directed to an
-      # index of tickets to that venue, and not see any
-      # tickets to other venues.
+      expect(current_path).to eq('/venue-1-name/events')
+
+      expect(page).to have_content(venue_1.name)
+      expect(page).to have_content(event_1.name)
+      expect(page).to have_content(event_2.name)
+      expect(page).to_not have_content(event_3.name)
     end
   end
 end
