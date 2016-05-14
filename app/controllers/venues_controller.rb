9ada6b63a891e5c6e9d530f6aad885f4294aca65
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-05-13 23:25:56 -0600

    Restore cart to order functionality

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,8 @@
 class VenuesController < ApplicationController
   def show
     @venue = Venue.find_by(slug: params[:venue])
+    @venues = Venue.all
     @events = @venue.events
+    @categories = Category.all
   end
 end
