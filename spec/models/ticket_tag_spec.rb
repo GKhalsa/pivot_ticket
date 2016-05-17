b0783bc8e8c9e9fe1e67ccce2eb86ac2f87b2a34
Drew Thompson <ferraritmnt@gmail.com>
2016-05-17 08:26:57 -0600

    add model tests for ticket_tags and tags

--- spec/models/ticket_tag_spec.rb
+++ spec/models/ticket_tag_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe TicketTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:ticket) }
+  it { is_expected.to belong_to(:tag) }
 end
