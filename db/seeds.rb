5a29546754500b3553e88b0fe9c206f5529de1ea
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-05-16 18:48:13 -0600

    Update seeds to conform to base data requirement

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,31 @@
 class Seed
 
+# 20 total businesses
+# 10 categories
+# 50 items per category
+# 100 registered customers, one with the following data:
+# Username: josh@turing.io
+# Password: password
+# 10 orders per registered customer
+# 1 business admins per business
+# Username: andrew@turing.io
+# Password: password
+# 1 platform administrators
+# Username: jorge@turing.io
+# Password: password
+
   def initialize
     create_roles
     create_known_users
     assign_roles
     create_sports_venue_event_tickets
     create_music_venue_event_tickets
+    create_users_with_orders
   end
 
   def create_roles
     puts "Creating Roles"
-    roles = ["registered_user", "venue_admin", "platform_admin"]
+    roles = ["registered_user", "buisness_admin", "platform_admin"]
     roles.each do |role|
       Role.create(name: role)
       puts role
@@ -18,7 +33,7 @@ class Seed
   end
 
   def create_known_users
-    users = ["platform_admin", "venue_admin", "user", "Josh", "Andrew", "Jorge", "Jeneve", "Drew", "Sunny"]
+    users = ["platform_admin", "buisness_admin", "user", "Josh", "Andrew", "Jorge", "Jeneve", "Drew", "Sunny"]
     users.each do |user|
       user = User.create(
       email: "#{user.downcase}@turing.io",
@@ -30,45 +45,50 @@ class Seed
 
   def assign_roles
     p_a = Role.find_by(name: "platform_admin")
-    v_a = Role.find_by(name: "venue_admin")
+    b_a = Role.find_by(name: "buisness_admin")
     r_u = Role.find_by(name: "registered_user")
     user = User.find_by(name: "platform_admin")
     user.roles = [p_a]
     puts "#{user.name} has #{user.roles.count} roles"
-    user = User.find_by(name: "venue_admin")
-    user.roles = [v_a]
+    user = User.find_by(name: "buisness_admin")
+    user.roles = [b_a]
     puts "#{user.name} has #{user.roles.count} roles"
     user = User.find_by(name: "user")
     user.roles = [r_u]
     puts "#{user.name} has #{user.roles.count} roles"
     user = User.find_by(name: "Josh")
-    user.roles = [p_a, v_a, r_u]
+    user.roles = [p_a, b_a, r_u]
     puts "#{user.name} has all #{user.roles.count} roles"
     user = User.find_by(name: "Andrew")
-    user.roles = [p_a, v_a, r_u]
+    user.roles = [p_a, b_a, r_u]
     puts "#{user.name} has all #{user.roles.count} roles"
     user = User.find_by(name: "Jorge")
-    user.roles = [p_a, v_a, r_u]
+    user.roles = [p_a, b_a, r_u]
     puts "#{user.name} has all #{user.roles.count} roles"
     user = User.find_by(name: "Drew")
-    user.roles = [p_a, v_a, r_u]
+    user.roles = [p_a, b_a, r_u]
     puts "#{user.name} has all #{user.roles.count} roles"
     user = User.find_by(name: "Sunny")
-    user.roles = [p_a, v_a, r_u]
+    user.roles = [p_a, b_a, r_u]
     puts "#{user.name} has all #{user.roles.count} roles"
     user = User.find_by(name: "Jeneve")
-    user.roles = [p_a, v_a, r_u]
+    user.roles = [p_a, b_a, r_u]
     puts "#{user.name} has all #{user.roles.count} roles"
   end
 
   def create_sports_venue_event_tickets
     sports = Category.create!(name: "Sports")
-    5.times do
+    10.times do
       sport_venue = Venue.create(name: "#{Faker::University.name} stadium",
                               address: "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state_abbr}, #{Faker::Address.zip}",
                                status: 1)
-      puts "Built #{sport_venue.name} at #{sport_venue.address}"
-      10.times do
+      user = User.create(name: "#{sport_venue.name} admin",
+                        email: "#{sport_venue.name}@turing.io",
+                     password: "password",
+                     venue_id: sport_venue.id)
+      user.roles = [Role.find_by(name: "buisness_admin")]
+      puts "Built #{sport_venue.name} at #{sport_venue.address} admin: #{sport_venue.users.first.name}"
+      5.times do
         team1 = Faker::Team.name
         team2 = Faker::Team.name
         game = Event.create(title: "#{team1} vs. #{team2} at #{sport_venue.name}",
@@ -78,7 +98,7 @@ class Seed
                       category_id: sports.id,
                          venue_id: sport_venue.id)
         puts "#{game.title} is happening on #{game.date}, at #{game.venue.name}!"
-        num_sports_tix = rand(75..200)
+        num_sports_tix = rand(50..75)
         num_sports_tix.times do
           Ticket.create(price: Faker::Commerce.price,
                 seat_location: Faker::Lorem.characters(3),
@@ -92,12 +112,17 @@ class Seed
 
   def create_music_venue_event_tickets
     music = Category.create!(name: "Music")
-    5.times do
+    10.times do
       music_venue = Venue.create(name: "#{Faker::Company.name} concert hall",
                               address: "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state_abbr}, #{Faker::Address.zip}",
                                status: 1)
-      puts "Built #{music_venue.name} at #{music_venue.address}"
-      10.times do
+      user = User.create(name: "#{music_venue.name} admin",
+                        email: "#{music_venue.name}@turing.io",
+                     password: "password",
+                     venue_id: music_venue.id)
+      user.roles = [Role.find_by(name: "buisness_admin")]
+      puts "Built #{music_venue.name} at #{music_venue.address} admin: #{music_venue.users.first.name}"
+      5.times do
         group = Faker::Superhero.name
         artist = Faker::Name.name
         concert = Event.create(title: "#{artist} with #{group} at #{music_venue.name}",
@@ -106,8 +131,9 @@ class Seed
                       event_image: Faker::Avatar.image,
                       category_id: music.id,
                          venue_id: music_venue.id)
+
         puts "#{concert.title} is happening on #{concert.date}, at #{concert.venue.name}!"
-        num_tix = rand(75..200)
+        num_tix = rand(50..75)
         num_tix.times do
           Ticket.create(price: Faker::Commerce.price,
                 seat_location: Faker::Lorem.characters(3),
@@ -117,6 +143,32 @@ class Seed
         puts "There are #{num_tix} tickets avaliable for #{Ticket.last.event.title} at #{Ticket.last.venue}."
       end
     end
+  end
+
+  def create_users_with_orders
+    puts "starting user count- #{User.count}"
+    puts "creating user with orders"
+    100.times do
+      user = User.create(name: Faker::Name.name,
+                        email: Faker::Internet.email,
+                     password: "password")
+      user.roles = [Role.find_by(name: "registered_user")]
+      10.times do
+        order = Order.create(user_id: user.id)
+        2.times do
+          ticket = Ticket.create(event_id: Event.first.id,
+                              category_id: Event.first.category_id,
+                                    price: Faker::Commerce.price,
+                            seat_location: Faker::Lorem.characters(3),
+                                  user_id: user.id
+                                  )
+          OrderTicket.create(ticket_id: ticket.id,
+                              order_id: order.id)
+        end
+      end
+    end
+    puts "created #{Order.count} orders, with #{OrderTicket.count} tickets"
+    puts "ending user count-#{User.count}"
   end
 
 end
