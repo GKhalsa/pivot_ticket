0446cd9108a3524b9be02699b106d26bd8d5836b
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-05-18 14:05:24 -0600

    add map marker data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,8 +21,9 @@ class EventsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@venue) do |venue, marker|
       marker.lat(venue.latitude)
       marker.lng(venue.longitude)
-      marker.infowindow("<img src='http://photos3.meetupstatic.com/photos/member/1/9/c/0/highres_30966592.jpeg' height='140' width='140'>".html_safe)
+      marker.infowindow("<img src='http://photos3.meetupstatic.com/photos/member/1/9/c/0/highres_30966592.jpeg' height='140' width='140'> #{venue.address}".html_safe)
     end
+    binding.pry
   end
 
   def index
